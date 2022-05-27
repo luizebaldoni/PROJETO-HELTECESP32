8a6566e6b7ce03273b1c5139adcb6855a896a4af
luizebaldoni <oliveira.luize@acad.ufsm.br>
2022-05-27 11:00:28 -0300

    corrigi erros no model//linhas 108 a 116

--- app/model.py
+++ app/model.py
@@ -34,11 +34,10 @@ def insert_rows(cursor: sqlite3.Cursor, table: str, tuples: list) -> None:
             else:
                 tuple_values += [str( v )]
 
-<<<<<<< HEAD
-        comando = "INSERT INTO %s(%s) VALUES (%s)" % (
-=======
+        comando = "INSERT INTO %s(%s) VALUES (%s)" % ()
+
         comando = "INSERT INTO %s(%s) VALUES (%s)" %(
->>>>>>> d9b76b8519d3ab55378c0970c97e19b922ae36a4
+
             table, ','.join( map( str, some_tuple.keys() ) ), ','.join( tuple_values ))
 
 def create_table(cursor: sqlite3.Cursor, tabela: str, dicionario: dict, chaveEst: list = None):
@@ -111,9 +110,9 @@ def main(path: str = '.', dbnome: str = 'test.db') -> None:
             cursor, 
             'SINAIS',  # tabela com 10 tuplas (no caso acho que tem mais)
             {
-                'id_sinais': ' INTEGER AUTOINCREMENT',
-                'data_sinais': 'datetime NOT NULL DEFAULT CURRENT TIMESTAMP',
-                'sinal_sensor':'float NOT NULL'})
+                'id_sinais': 'INTEGER PRIMARY KEY AUTOINCREMENT',
+                'data_sinais': 'DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP',
+                'sinal_sensor':'FLOAT NOT NULL'})
       
 if __name__ == '__main__':
     main()
