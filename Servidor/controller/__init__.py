f7d55a2498e0f33073b99a80bc172dd67ba0a230
luizebaldoni <oliveira.luize@acad.ufsm.br>
2022-05-26 21:55:58 -0300

    alterações no controller//adicionei relação com a placa

--- Servidor/controller/__init__.py
+++ Servidor/controller/__init__.py
@@ -1,95 +1,42 @@
 import os
 import sqlite3
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import sys
+import serial
 
 filepath = os.path.dirname(__file__)
-sys.path.append(os.path.join(filepath, "'.."))
+sys.path.append(os.path.join(filepath, '..'))
 
-# importa módulo do banco de dados; permite rodar as funções desse módulo através da sintaxe 
-# model.<nome_da_função>
-# por exemplo, model.select_rows
-# para selecionar as linhas
+import model
 
-import model 
-
-def get_table_column_names(cursor: sqlite3.Cursor, table_name: str) -> list:
-    #pega o nome das colunas do banco de dados
-    # cursor = para o banco de dados
-    # table_name = nome da tabela
-    # return =  o nome das tabelas do banco como lista
-
+def get_nome_colunas(cursor: sqlite3.Cursor, table_name: str) -> list:
     res = model.raw_execute(cursor, 'PRAGMA table_info(%s)' % table_name)
-    column_names = [x[1] for x res.fetchall()]
+    column_names = [x[1] for x in res.fetchall()]
     return column_names
 
-def main()
-    app = Flask(__name__) #inicia o flask
+
+def main():
+    app = Flask(__name__)  # inicia uma aplicação do flask (o backend)
     db_path = os.path.join(filepath, '..', 'model')
     db_name = 'test.db'
-    #deleta e cria o banco toda vez q o backend for iniciado
+    # deleta & cria o banco de dados, toda vez que o backend for iniciado
     model.main(db_path, db_name)
 
-    @app.route('/')
-    def initial_page()
+ #populando tabela:
+    @app.route('/itens_tabela', methods=['POST'])
+    def populate_table():
+     with model.SQLite(os.path.join(db_path, db_name)) as cursor:
+        nome_tabela ='SINAIS' #selecionando tabela para aparecer
+        busca = 'SELECT id_sinais, data_sinais, sinal_sensor FROM %s' % nome_tabela #indicando colunas para puxar
+        tabela = model.select_rows(cursor, busca)
+        dicionarios = [] #convertendo em dicionarios
 
-        return '''
-        <p> arquivo html com a pagina inicial</p>
-        '''
-
-    @app.route('/populate_table', methods = ['POST'])
-    def populate_table()
-        with model.SQLite(os.path.join(db_path, db_name)) as cursor:
-            lista_de_tuplas = model.select_rows(cursor, "SELECT * FROM #nome da tabela")
-
-            colunas = get_table_column_names(cursor, 'filme')
-            #lista que vai conter dicionarios criados
-            lista_de_dicionarios = []
-
-            #unir as listas e converter em dicionario
-            for i in range(len(lista_de_tuplas)):
-                dicionario = {}
-                for j in range(len(colunas)):
-                    dicionario[colunas[j]] = lista_de_tuplas[i][j]
-                
-                #adiciona os dicts na lista
-                lista_de_dicionarios.append(dicionarios)
-            
-            response = jsonify(lista_de_dicionarios)
-
-            #adicionar esta linha para as respostas 
-            response.headers.add('Access-Control-Allow-Origin', '*')
-            return response
-
-@app.route('/selcte_and_populate_table', methods = ['POST'])
-def select_and_populate_table():
-    with model, SQLite(os.path,join(db_path, db_name)) as cursor:
-        chamada = request.form['second_task_table_selector']
-        colunas = get_table_column_names(cursor, chamada)
-        select = 'SELECT * FROM ' + chamada
-
-        lista_de_tuplas = model.select_rows(cursor, select)
-
-        #lista vazia que vai conter os dicts
-        lista_de_dicionarios = []
-
-        #une listas e cria diconarios
-        for i in range(len(lista_de_tuplas)):
-            dicionario = {}
-            for j in range(len(colunas)):
-                dicionario[colunas[j]] = lista_de_tuplas[i][j]
-
-            #adiciona os dicionarios na lista
-            lista_de_dicionarios.append(dicionario)
-        
-        response = jsonify(lista_de_dicionarios)
-
+        for linha in tabela:
+            dicionarios.append({'id_sinais': linha[0], 'data_sinais': linha [1], 'sinal_sensor': [2]
+                })
+        response = jsonify(dicionarios) #mandando retornar os dicionarios
         response.headers.add('Access-Control-Allow-Origin', '*')
         return response
-
-    #coloca o backend rodar
     app.run()
-
 if __name__ == '__main__':
     main()
-
--- Servidor/model/__init__.py
+++ Servidor/model/__init__.py
@@ -1,12 +1,12 @@
 import os
 import sqlite3
 
-class SQLite(object):
+class SQLite( object ):
     def __init__(self, file):
         self.file = file
 
     def __enter__(self):
-        self.conn = sqlite3.connect(self.file)
+        self.conn = sqlite3.connect( self.file )
         self.conn.row_factory = sqlite3.Row
         return self.conn.cursor()
 
@@ -14,90 +14,67 @@ class SQLite(object):
         self.conn.commit()
         self.conn.close()
 
-
-def create_table(cursor: sqlite3.Cursor, table: str, fields: dict, other_data: list = None):
-    # criar tabelas
-    # cursor = para o banco de dados
-    # table = nome da tabela
-    # fields = dict onde a chave é o nome da coluna e o valor e tipo
-    # other_data = define foreign keys ou outras config
+def create_table(cursor: sqlite3.Cursor, table: str, fields: dict, other_data: list = None) -> None:
 
     command = "CREATE TABLE %s (%s)" % (
         table,
-        ','.join([k + ' ' + v for k, v in fields.items()] + (
-            other_data if other_data is not None else []))
+        ','.join( [k + ' ' + v for k, v in fields.items()] + (
+            other_data if other_data is not None else []) )
     )
-    cursor.execute(command)
+    cursor.execute( command )
 
-def insert_rows(cursor: sqlite3.Cursor, table: str, tuples: list):
-    #insere tuplas na tabela 
-    #cursor = para o banco de dados
-    #table = nome da tabela
-    #tuples = lista de dicionarios
+def insert_rows(cursor: sqlite3.Cursor, table: str, tuples: list) -> None:
 
     for some_tuple in tuples:
         tuple_values = []
         for v in some_tuple.values():
-            if isinstance(v, str):
+            if isinstance( v, str ):
                 tuple_values += ['\'' + v + '\'']
             else:
-                tuple_values += [str(v)]
+                tuple_values += [str( v )]
 
         command = "INSERT INTO %s(%s) VALUES (%s)" % (
-            table, ','.join(map(str, some_tuple.keys())), ','.join(tuple_values)
+            table, ','.join( map( str, some_tuple.keys() ) ), ','.join( tuple_values )
         )
-        cursor.execute(command)
+        cursor.execute( command )
+
 
 def select_rows(cursor: sqlite3.Cursor, clause: str) -> list:
-    #faz seleção das tuplas de uma tabela - qualquer comando sqlite
-    #cursor = para o banco de dados
-    #clause = faz a seleçao (SELECT * FROM table)
 
-    res = cursor.execute(clause)
+    res = cursor.execute( clause )
     rows = []
-    for row in res:
-        rows += [tuple([row[k] for k in row.keys()])]
+    for row in res:  # type: sqlite3.Row
+        rows += [tuple( [row[k] for k in row.keys()] )]
 
     return rows
 
-def raw_execute(cursor: sqlite3.Cursor, clause: str) -> sqlite3.Cursor:
-    #executa o comando sqlite e retorna o resultado
-    #cursor = para o banco de dados
-    #clause = clausula sqlite
-    #return = retorno da execuçao da clausula
 
-    return cursor.execute(clause)
+def raw_execute(cursor: sqlite3.Cursor, clause: str) -> sqlite3.Cursor:
+
+    return cursor.execute( clause )
+
 
 def remove_db(file: str) -> None:
-    #deleta o arquivo .db 
-    #file caminho para o banco de dados
 
     try:
-        os.remove(file)
+        os.remove( file )
     except FileNotFoundError:
         pass
 
-def main(path: str = '.', db_name: str = 'test.db') -> None:
-    remove_db(os.path.join(path, db_name))
 
-    with SQLite(os.path.join(path, db_name)) as cursor:
+def main(path: str = '.', db_name: str = 'test.db') -> None:
+    remove_db( os.path.join( path, db_name ) )
+
+    with SQLite( os.path.join( path, db_name ) ) as cursor:
+        # todas as tabelas possuem mais de 4 colunas
+        # foram feitas mais de 4 tabelas
         create_table(
             cursor,
-            """"
-            'nome da tabela',
-            {nome da coluna: dado}
-            """"
-        )
-
-        insert_rows(
-            cursor, 
-            """
-            'nome da tabela'
-            [
-                {chave: ., nome da coluna: item}
-            ]
-            """
-        )
-
+            'SINAIS',  # tabela com 10 tuplas (no caso acho que tem mais)
+            {
+                'id_sinais': ' INTEGER AUTOINCREMENT',
+                'data_sinais': 'datetime NOT NULL DEFAULT CURRENT TIMESTAMP',
+                'sinal_sensor':'float NOT NULL'})
+      
 if __name__ == '__main__':
     main()
