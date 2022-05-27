565ac1fc7962b14aee04278fde50aa43e5741279
luizebaldoni <oliveira.luize@acad.ufsm.br>
2022-05-27 20:57:53 -0300

    corrigi as pastas // modifiquei main.html // adicionei o codigo heltec

---
+++ app/controller/static/main.js
@@ -0,0 +1,74 @@
+$("#itens_tabela").on("submit", function(event){
+    event.preventDefault(); //this prevents the form to use default submit
+ 
+     $.ajax({
+         method: "POST",
+         url: $(this).attr("action"), //this will use the form's action attribute
+         data: {populate_table: $("#itens_tabela").val()},
+         success: function(responseData) {
+             let res = JSON.stringify(responseData);
+ 
+             let response = eval(responseData);
+ 
+             let table = document.createElement('table');
+ 
+             for(let i = 0; i < response.length; i++) {
+                 // builds header
+                 if(i === 0) {
+                     let tr = table.insertRow();
+                     for(let key in response[i]) {
+                         let td = tr.insertCell();
+                         let bold = document.createElement('strong');
+                         let some_node = document.createTextNode(key);
+                         bold.appendChild(some_node);
+                         td.appendChild(bold);
+                     }
+                 }
+                 // builds rest
+                 let tr = table.insertRow();
+                 for(let key in response[i]) {
+                     let td = tr.insertCell();
+                     td.appendChild(document.createTextNode(response[i][key]));
+                 }
+             }
+             $('#first_task_response_table').html(table);
+         }
+     });
+ });
+ $("#seleciona_tabela").on("submit", function(event){
+    event.preventDefault(); //this prevents the form to use default submit
+ 
+     $.ajax({
+         method: "POST",
+         url: $(this).attr("action"), //this will use the form's action attribute
+         data: {second_task_table_selector: $("#table_selector").val()},
+         success: function(responseData) {
+             let res = JSON.stringify(responseData);
+ 
+             let response = eval(responseData);
+ 
+             let table = document.createElement('table');
+ 
+             for(let i = 0; i < response.length; i++) {
+                 // builds header
+                 if(i === 0) {
+                     let tr = table.insertRow();
+                     for(let key in response[i]) {
+                         let td = tr.insertCell();
+                         let bold = document.createElement('strong');
+                         let some_node = document.createTextNode(key);
+                         bold.appendChild(some_node);
+                         td.appendChild(bold);
+                     }
+                 }
+                 // builds rest
+                 let tr = table.insertRow();
+                 for(let key in response[i]) {
+                     let td = tr.insertCell();
+                     td.appendChild(document.createTextNode(response[i][key]));
+                 }
+             }
+             $('#table_selector').html(table);
+         }
+     });
+ });
