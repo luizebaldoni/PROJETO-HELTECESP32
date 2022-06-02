cdb5fd10fc17591cf746bf1917b14cd94b2292b2
luizebaldoni <oliveira.luize@acad.ufsm.br>
2022-06-02 15:25:34 -0300

    modifiquei o cosifo teste do web server de agyua

--- Codigos/codigo_agua_web_server_teste02/codigo_agua_web_server_teste02.ino
+++ Codigos/codigo_agua_web_server_teste02/codigo_agua_web_server_teste02.ino
@@ -2,21 +2,21 @@
 #include <WiFiClient.h>
 #include <WiFiAP.h>
 #include <WebServer.h>
-#include <ESPmDNS.h>
+#include <heltec.h>
 
 const char *ssid = "ESP32";
 const char *password = "Teste03";
 //DEFINICOES
-const int pinosensor = 4;       // Pino conectado ao sensor
-const int pinoLED = 12; // Pino conectado à LED
+const int pinosensor = 4;      
+const int pinoLED = 12; 
 WiFiServer server(80);
 //CODIGO PRINCIPAL
 void setup() {
-  Serial.begin(115200); //Inicia comunicação serial
+  Serial.begin(115200); 
   Serial.println();
   Serial.println("configurando...");    
-  delay(6000); // Aguarda o sensor armar       
-  pinMode(pinosensor, INPUT); //DEFINE O PINO COMO ENTRADA
+  delay(6000); 
+  pinMode(pinosensor, INPUT); 
   pinMode(pinoLED, OUTPUT); //DEFINE O PINO COMO SAÍDA    
   WiFi.softAP(ssid, password); // DEFINE que para ter acesso precisa senha
   IPAddress myIP = WiFi.softAPIP();
@@ -29,20 +29,15 @@ void setup() {
 }
 //CONDICOES
 void loop() {
-  if(analogRead(pinosensor) > 690){ //SE A LEITURA DO PINO FOR MAIOR QUE 690 BITS (PODE SER AJUSTADO), EXECUTA:
-      printf("A leitura do sensor é amior q 690 bits"); //ACENDE O LED
-  }else{ //SENÃO, EXECUTA:
-    digitalWrite(pinoLED, LOW); //DESLIGA O LED
-  }
   WiFiClient client = server.available(); 
-  if (client) { // se você conseguir um cliente,
-  Serial.println("Novo Cliente."); // imprime uma mensagem pela porta serial
-  String currentLine = ""; // faz uma String para manter os dados recebidos do cliente
-  while (client.connected()) { // loop enquanto o cliente está conectado
-  if (client.available()) { // se houver bytes para ler do cliente,
-  char c = client.read(); // leia um byte e depois
-  Serial.write(c); // imprima o monitor serial
-  if (c == '\n') { // se o byte for um caractere de nova linha
+  if (client) { 
+  Serial.println("Novo Cliente.");
+  String currentLine = ""; 
+  while (client.connected()) { 
+  if (client.available()) { 
+  char c = client.read(); 
+  Serial.write(c); 
+  if (c == '\n') { 
   if (currentLine.length() == 0) {
     client.println("HTTP/1.1 200 OK");
     client.println("Content-type:text/html");
@@ -52,9 +47,7 @@ void loop() {
     client.println("<h1>Recebendo dados da porta analogica</h1>");
     client.println("<p>Valor da porta analógica:</p>");
     if (analogRead(pinosensor) > 690){ //SE A LEITURA DO PINO FOR MAIOR QUE 690 BITS (PODE SER AJUSTADO), EXECUTA:
-      client.println("<p>A leitura do sensor é maior q 690 bits</p>");
-      }else if (analogRead(pinosensor) < 690){ 
-        client.println("<p>A leitura do sensor é menor do que a esperada</p>"); 
+      client.println("<p>A leitura do sensor eh maior q 690 bits</p>");
       }else{ 
         client.println("<p>Nao foi possivel ler o sensor!</p>");
       }
