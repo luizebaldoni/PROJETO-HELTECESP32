8edf628b70c3c8d7e773e42a1e1462a06c4b38a9
mariadesconci <mariaeduardawdesc@gmail.com>
2022-06-23 15:22:52 -0300

    codigo sensor fotoluminescente

---
+++ Codigos/codigofotolum_test01/codigofotolum_test01.ino
@@ -0,0 +1,22 @@
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WiFiAP.h>
+#include <WebServer.h>
+#include <heltec.h>
+
+const int pinoLed = 13; //PINO DIGITAL UTILIZADO PELO LED
+const int pinoLDR = 4; //PINO ANALÓGICO UTILIZADO PELO LDR
+  
+void setup(){  
+  pinMode(pinoLed, OUTPUT); //DEFINE O PINO COMO SAÍDA
+  pinMode(pinoLDR, INPUT); //DEFINE O PINO COMO ENTRADA
+}    
+void loop(){
+  //O VALOR 600 PODE SER AJUSTADO
+  if(analogRead(pinoLDR) > 600){ //SE O VALOR LIDO FOR MAIOR QUE 600, FAZ
+    digitalWrite(pinoLed, HIGH); //ACENDE O LED
+  }  
+  else{ //SENÃO, FAZ
+    digitalWrite(pinoLed, LOW); //APAGA O LED
+  }  
+}
