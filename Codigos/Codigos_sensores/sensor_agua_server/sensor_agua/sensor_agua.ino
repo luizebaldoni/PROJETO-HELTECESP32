dc262e855e6393eaf93f70b0526dd5e33ac85ac0
luizebaldoni <oliveira.luize@acad.ufsm.br>
2022-07-26 14:20:52 -0300

    adicionei codigo web server do sensor de agua// FUNCIONANDO

--- Codigos/Codigos_sensores/sensor_agua_server/sensor_agua/sensor_agua.ino
+++ Codigos/Codigos_sensores/sensor_agua_server/sensor_agua/sensor_agua.ino
@@ -7,7 +7,6 @@ int pinosensor = 4;       // Pino conectado ao sensor
 int pinoLED = 12; // Pino conectado à LED
 
 //CODIGO PRINCIPAL
-String sinais; // Variável para enviar os sinais
 void setup() {
   Serial.begin(9600); //Inicia comunicação serial    
   delay(6000); // Aguarda o sensor armar       
@@ -19,6 +18,7 @@ void setup() {
 void loop() {
   if(analogRead(pinosensor) > 690){ //SE A LEITURA DO PINO FOR MAIOR QUE 690 BITS (PODE SER AJUSTADO), EXECUTA:
       digitalWrite(pinoLED, HIGH); //ACENDE O LED
+      printf("led acesa \n");
       delay(5000);
   }else{ //SENÃO, EXECUTA:
     digitalWrite(pinoLED, LOW); //DESLIGA O LED
--- Codigos/Webserver/webserver_teste03_led/webserver_teste03_led/webserver_teste03_led.ino
+++ Codigos/Webserver/webserver_teste03_led/webserver_teste03_led/webserver_teste03_led.ino
@@ -5,33 +5,29 @@
 #include <ESPmDNS.h>
 #include <heltec.h>
 
-const char *ssid = "ESP32";
-const char *password = "Teste03";
+const char *ssid = "ESP32"; // nome da rede
+const char *password = "carrinho"; // senha para acessar a rede
 //DEFINICOES 
-const int pino = 12;
-const int sensor = 4;
-  int valor_sensor = analogRead(sensor);
-  int valor_led = digitalRead(pino);
+  int valor_sensor = analogRead(4); // pino analogico do sensor
 WiFiServer server(80);
 //CODIGO PRINCIPAL
 void setup() {
-  Serial.begin(115200); 
+  Serial.begin(115200); // velocidade serial
   Serial.println();
   Serial.println("configurando...");    
   delay(6000); 
-  pinMode(12, OUTPUT); //DEFINE O PINO COMO SAÍDA 
   pinMode(4, INPUT);
   WiFi.softAP(ssid, password); // DEFINE que para ter acesso precisa senha
   IPAddress myIP = WiFi.softAPIP();
-  Serial.print("O Endereço IP Da Rede : ");
+  Serial.print("O Endereço IP Da Rede : "); // mostrando no monitor serial o endereço do server
   Serial.println(myIP);
   server.begin(); //inicia o webserver
   Serial.println("Servidor Iniciado!!");
 
 
 }
-//CONDICOES
 void loop() {
+  // abrindo coneccao cliente/servidor
   WiFiClient client = server.available(); 
   if (client) { 
     Serial.println("Novo Cliente.");
@@ -42,6 +38,7 @@ void loop() {
       Serial.write(c); 
   if (c == '\n') { 
   if (currentLine.length() == 0) {
+    // criando html do setrvidor
     client.println("HTTP/1.1 200 OK");
     client.println("Content-type:text/html");
     client.println();
@@ -50,21 +47,15 @@ void loop() {
     client.print("<body>");
     client.print("<style type=\"text/css\">h1{color: black;font-family: 'Times New Roman', Times, serif; }.ligado{background-color: green;color: aliceblue;}</style>");
     client.print("<h1>RECEPCAO DE DADOS</h1>");
-    client.print("<p>Informacoes da LED: </p>");
-    if (valor_led == true){ //SE A LEITURA DO PINO FOR MAIOR QUE 690 BITS (PODE SER AJUSTADO), EXECUTA:
-    client.println("<li>A LED esta ligada</li>");
-      }else{ 
-        client.println("<li>A LED esta desligada!</li>");
-      }
       client.print("<p>Informacoes do sensor de agua:</p>");
-     if (valor_sensor == true){
-      client.println("<li>A leitura do sensor e maior que 690 bits</li>");
-      }else{
-        client.print("<li>A leitura do sensor e menor que 690 bits ou sensor desligado!</li>");
+     if (valor_sensor >= 690){ // colocando a condiçao que faz o sensor funcionar
+      client.println("<li>A leitura do sensor e maior que 690 bits</li>"); // imprime na tela
+      }else{ // se nao for a condicao inicial:
+        client.print("<li>A leitura do sensor e menor que 690 bits ou sensor desligado!</li>"); // imprime na tela
         }
     client.println("</html>");
     client.println("</body>");
-    client.stop();
+    client.stop(); // fecha coneccao com o cliente
 }
   }
   }
